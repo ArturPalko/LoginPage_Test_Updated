482a4459ec0036edc5c2a453af62ed3c8d653ae2
Artur Palko <super.gudo32@gmail.com>
2024-08-22 21:39:18 +0300

    second commit

--- Test.cs
+++ Test.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLog;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium;
-using System;
 using WebdriverFactory;
 using Webdriver.FinalTestTak;
-using ExecuteTestInParallel; 
+using System;
 
 namespace LoginPageTests
 {
@@ -15,6 +11,25 @@ namespace LoginPageTests
     public class LoginPageTests
     {
         public static Logger Logger = LogManager.GetCurrentClassLogger();
+        private IWebDriver _driver;
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            Logger.Info("Tearing down the test environment.");
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver.Dispose();
+            }
+            Logger.Info("Browser closed.");
+        }
+        public void InitDriver(BrowserType browserType)
+        {
+            IWebDriverFactory factory = WebDriverFactoryProvider.GetFactory(browserType);
+            _driver = factory.CreateDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
 
         [TestMethod]
         [DataRow(BrowserType.Chrome)]
@@ -22,30 +37,28 @@ namespace LoginPageTests
         [DataRow(BrowserType.Edge)]
         public void LoginFormWithEmptyCredentials(BrowserType browserType)
         {
-            ParallelTest.execute(_driver =>
-            {
-                Logger.Info("Starting the test setup.");
-                LoginPage _loginPage = new LoginPage(_driver);
-                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            // Arrange (Given)
+            Logger.Info($"Starting the test in {browserType}.");
+            InitDriver(browserType);
+            LoginPage _loginPage = new LoginPage(_driver);
 
-                Logger.Info("Starting LoginFormWithEmptyCredentials test.");
+            Logger.Info("Given the login page is open");
+            _loginPage.Open();
 
-                _loginPage.Open();
-                Logger.Info("Opened login page.");
+            // Act (When)
+            Logger.Info("When I fill out the form with empty credentials");
+            _loginPage.FillOutTheForm();
 
-                _loginPage.FillOutTheForm();
-                Logger.Info("Filled out the login form with empty credentials.");
+            Logger.Info("And I clear the form");
+            _loginPage.ClearTheForm();
 
-                _loginPage.ClearTheForm();
-                Logger.Info("Cleared the form.");
+            Logger.Info("And I submit the form");
+            _loginPage.SubmitTheForm();
 
-                _loginPage.SubmitTheForm();
-                Logger.Info("Submitted the form.");
-
-                _loginPage.CheckErrorMessage(LoginPage.ErrorMessageUsernameIsRequired);
-                Logger.Info("Checked error message for empty credentials.");
-            }, browserType);
-        } 
+            // Assert (Then)
+            Logger.Info("Then I should see an error message indicating that the username is required");
+            _loginPage.CheckErrorMessage(LoginPage.ErrorMessageUsernameIsRequired);
+        }
 
         [TestMethod]
         [DataRow(BrowserType.Chrome)]
@@ -53,29 +66,27 @@ namespace LoginPageTests
         [DataRow(BrowserType.Edge)]
         public void LoginFormWithCredentialsByPassingUsername(BrowserType browserType)
         {
-            ParallelTest.execute(_driver =>
-            {
-                Logger.Info("Starting the test setup.");
-                LoginPage _loginPage = new LoginPage(_driver);
-                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            // Arrange (Given)
+            Logger.Info($"Starting the test in {browserType}.");
+            InitDriver(browserType);
+            LoginPage _loginPage = new LoginPage(_driver);
 
-                Logger.Info("Starting LoginFormWithCredentialsByPassingUsername test.");
+            Logger.Info("Given the login page is open");
+            _loginPage.Open();
 
-                _loginPage.Open();
-                Logger.Info("Opened login page.");
+            // Act (When)
+            Logger.Info("When I fill out the form with username but without password");
+            _loginPage.FillOutTheForm(LoginPage.AcceptedUserName, LoginPage.AcceptedPassword);
 
-                _loginPage.FillOutTheForm(LoginPage.AcceptedUserName, LoginPage.AcceptedPassword);
-                Logger.Info("Filled out the form with username but without password.");
+            Logger.Info("And I clear the password field");
+            _loginPage.ClearThePasswordField();
 
-                _loginPage.ClearThePasswordField();
-                Logger.Info("Cleared the password field.");
+            Logger.Info("And I submit the form");
+            _loginPage.SubmitTheForm();
 
-                _loginPage.SubmitTheForm();
-                Logger.Info("Submitted the form.");
-
-                _loginPage.CheckErrorMessage(LoginPage.ErrorMessagePasswordIsRequired);
-                Logger.Info("Checked error message for missing password.");
-            }, browserType);
+            // Assert (Then)
+            Logger.Info("Then I should see an error message indicating that the password is required");
+            _loginPage.CheckErrorMessage(LoginPage.ErrorMessagePasswordIsRequired);
         }
 
         [TestMethod]
@@ -84,30 +95,28 @@ namespace LoginPageTests
         [DataRow(BrowserType.Edge)]
         public void LoginFormWithCredentialsByPassingUsernameAndPassword(BrowserType browserType)
         {
-            ParallelTest.execute(_driver =>
+            // Arrange (Given)
+            Logger.Info($"Starting the test in {browserType}.");
+            InitDriver(browserType);
+            LoginPage _loginPage = new LoginPage(_driver);
 
-            {
-                LoginPage _loginPage = new LoginPage(_driver);
-                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Logger.Info("Given the login page is open");
+            _loginPage.Open();
 
-                Logger.Info("Starting LoginFormWithCredentialsByPassingUsernameAndPassword test.");
+            // Act (When)
+            Logger.Info("When I fill out the form with valid username and password");
+            _loginPage.FillOutTheForm(LoginPage.AcceptedUserName, LoginPage.AcceptedPassword);
 
-                _loginPage.Open();
-                Logger.Info("Opened login page.");
+            Logger.Info("And I submit the form");
+            _loginPage.SubmitTheForm();
 
-                _loginPage.FillOutTheForm(LoginPage.AcceptedUserName, LoginPage.AcceptedPassword);
-                Logger.Info("Filled out the form with valid username and password.");
-
-                _loginPage.SubmitTheForm();
-                Logger.Info("Submitted the form.");
-
-                _loginPage.CheckLogin(LoginPage.SwagLabsPageUrl);
-                Logger.Info("Checked login success and redirected to the main page.");
-            }, browserType);
-
+            // Assert (Then)
+            Logger.Info("Then I should be redirected to the main page and logged in successfully");
+            _loginPage.CheckLogin(LoginPage.SwagLabsPageUrl);
         }
     }
 }
+
 
 
 
